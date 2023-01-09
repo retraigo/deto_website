cefb3defde70294ac4fa31aa28e61453cb286197
Neko Of The Abyss <psi@innocent.com>
2023-01-09 16:55:41 +0530

    a component just in case

---
+++ components/bg/CircleGrid.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class = "w-full relative overflow-hidden">
+        <div class = "absolute inset-0 w-full flex flex-wrap gap-0 z-0">
+            <div v-for = "(_x, i) in elements" :key="`grid_item_${i}`" class = "w-12 h-12 rounded-full bg-transparent hover:bg-royal-yellow/20 transition duration-100 ease-in-out" />
+
+        </div>
+        <div class = "relative z-10 pointer-events-none"><slot /></div>
+    </div>
+</template>
+<script setup lang="ts">
+const elements = new Array(225).fill(0)
+</script>
