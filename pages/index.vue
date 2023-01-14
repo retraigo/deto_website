453972e747f0ce3c50c10068595f78e6c5a0a042
Neko Of The Abyss <psi@innocent.com>
2023-01-14 23:04:13 +0530

    desc

--- pages/index.vue
+++ pages/index.vue
@@ -22,19 +22,11 @@
                 data-aos-delay="100"
                 data-aos-duration="260"
             >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                sed eros vel ante blandit interdum. In lacus enim, luctus
-                scelerisque eros nec, convallis blandit nisi. Etiam imperdiet,
-                purus sed gravida hendrerit, odio odio condimentum mauris, ut
-                iaculis quam augue vitae justo. Cras tincidunt mattis odio nec
-                ultricies. Etiam tincidunt ex odio, nec efficitur tortor aliquam
-                vitae. Aenean quis tempus nisl. Etiam vel velit risus. Orci
-                varius natoque penatibus et magnis dis parturient montes,
-                nascetur ridiculus mus. Nullam nec odio eget magna commodo
-                pulvinar. Donec vehicula libero eget est interdum, et lobortis
-                libero rutrum. Nunc lobortis condimentum magna at porttitor.
-                Nulla finibus ac purus ultricies sollicitudin. Phasellus a
-                viverra elit. Curabitur consectetur at ligula eget aliquet.
+                An annual tech festival hosted by the department of Artificial
+                Intelligence and Data Science of Easwari Engineering College in
+                association with SCARDS (Student Council of Artificial
+                Intelligence & Data Science). We conduct a wide range of both
+                technical and non-technical events
             </div>
             <div
                 class="flex flex-col lg:flex-row items-center gap-2 max-w-7xl mx-auto prose dark:prose-dark text-justify"
