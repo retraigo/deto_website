690db545556419dc3b960449f74c31fac26f70b2
0Enigma <0Enigma@1>
2023-01-31 21:25:32 +0530

    change whatsapp verification

--- components/ui/Fab.vue
+++ components/ui/Fab.vue
@@ -14,7 +14,6 @@
                 @click="toggle"
             >
                 <span :class="`sr-only`">Open menu</span>
-                <img src="/EEC_ICON.webp" class="hidden w-12 h-12" />
                 <svg
                     :class="`block lg:hidden h-8 w-8 text-white stroke-2 stroke-white`"
                     xmlns="http://www.w3.org/2000/svg"
