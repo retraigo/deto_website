8577c84de9b556faf19b81e461268bd3181feb35
retraigo <nett@nett.moe>
2023-02-03 16:14:51 +0530

    update events formatting

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,24 +1,17 @@
 <template>
     <NuxtLayout>
-        <section class="mx-auto space-y-8">
-        <article class="lg:flex flex-1 w-full mx-auto mt-8">
+        <section class="lg:flex flex-1 w-full mx-auto mt-8 max-w-7xl">
             <div class="flex-1 w-full flex flex-col gap-8 items-center">
                 <div class="p-8">
-                    <h1
-                        class="text-center text-4xl font-bold font-azonix mx-auto"
-                    >
+                    <h1 class="text-center text-4xl font-bold font-atmospheric mx-auto" data-aos="fade-up"
+                        data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
                         Technical Events
                     </h1>
-                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-6 w-full">
-                        <a
-                            v-for="event in technical"
-                            :key="event._id"
-                            :href="event._path"
-                            class="flex flex-col gap-4 items-center w-full lg:max-w-md p-8 border rounded-xl border-zinc-700 dark:border-royal-yellow bg-black/60 transition duration-500 ease-in-out transform hover:-translate-y-1"
-                        >
-                            <div
-                                class="uppercase text-base text-royal-yellow font-semibold text-center"
-                            >
+                    <div class="flex flex-row flex-wrap items-stretch justify-center gap-8 mt-6">
+                        <a v-for="event in technical" :key="event._id" :href="event._path"
+                            class="flex flex-col gap-4 items-center w-full lg:max-w-xl p-8 border-2 border-zinc-700 dark:border-royal-yellow bg-black/60 transition duration-500 ease-in-out transform hover:-translate-y-1"
+                            data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
+                            <div class="uppercase text-base text-royal-yellow font-semibold text-center font-azonix">
                                 {{ event.title }}
                             </div>
                             <div class="text-lg text-center">
@@ -28,23 +21,15 @@
                     </div>
                 </div>
                 <div class="p-8">
-                    <h1
-                        class="text-center text-4xl font-bold font-azonix mx-auto"
-                    >
+                    <h1 class="text-center text-4xl font-bold font-atmospheric mx-auto" data-aos="fade-up"
+                        data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
                         Non-Technical Events
                     </h1>
-                    <div
-                        class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-6 w-full"
-                    >
-                        <a
-                            v-for="event in nonTechnical"
-                            :key="event._id"
-                            :href="event._path"
-                            class="flex flex-col gap-4 items-center w-full lg:max-w-md p-8 border rounded-xl border-zinc-700 dark:border-royal-yellow bg-black/60 transition duration-500 ease-in-out transform hover:-translate-y-1"
-                        >
-                            <div
-                                class="uppercase text-base text-royal-yellow font-semibold text-center"
-                            >
+                    <div class="flex flex-row flex-wrap items-stretch justify-center gap-8 mt-6">
+                        <a v-for="event in nonTechnical" :key="event._id" :href="event._path"
+                            class="flex flex-col gap-4 items-center w-full lg:max-w-xl p-8 border-2 border-zinc-700 dark:border-royal-yellow bg-black/60 transition duration-500 ease-in-out transform hover:-translate-y-1"
+                            data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
+                            <div class="uppercase text-base text-royal-yellow font-semibold text-center font-azonix">
                                 {{ event.title }}
                             </div>
                             <div class="text-lg text-center">
@@ -54,23 +39,15 @@
                     </div>
                 </div>
                 <div class="p-8">
-                    <h1
-                        class="text-center text-4xl font-bold font-azonix mx-auto"
-                    >
+                    <h1 class="text-center text-4xl font-bold font-atmospheric mx-auto" data-aos="fade-up"
+                        data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
                         Online Events
                     </h1>
-                    <div
-                        class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-6 w-full"
-                    >
-                        <a
-                            v-for="event in online"
-                            :key="event._id"
-                            :href="event._path"
-                            class="flex flex-col gap-4 items-center w-full lg:max-w-md p-8 border rounded-xl border-zinc-700 dark:border-royal-yellow bg-black/60 transition duration-500 ease-in-out transform hover:-translate-y-1"
-                        >
-                            <div
-                                class="uppercase text-base text-royal-yellow font-semibold text-center"
-                            >
+                    <div class="flex flex-row flex-wrap items-stretch justify-center gap-8 mt-6">
+                        <a v-for="event in onlineEvents" :key="event._id" :href="event._path"
+                            class="flex flex-col gap-4 items-center w-full lg:max-w-xl p-8 border-2 border-zinc-700 dark:border-royal-yellow bg-black/60 transition duration-500 ease-in-out transform hover:-translate-y-1"
+                            data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
+                            <div class="uppercase text-base text-royal-yellow font-semibold text-center font-azonix">
                                 {{ event.title }}
                             </div>
                             <div class="text-lg text-center">
@@ -80,45 +57,48 @@
                     </div>
                 </div>
             </div>
-        </article>
         </section>
     </NuxtLayout>
 </template>
 
 <style lang="postcss">
-    /* Customize headers to remove default underline */
-    .prose h1 {
-        @apply text-royal-orange;
-        &:hover {
-            @apply text-zinc-600 dark:text-royal-yellow;
-        }
+/* Customize headers to remove default underline */
+.prose h1 {
+    @apply text-royal-orange;
+
+    &:hover {
+        @apply text-zinc-600 dark:text-royal-yellow;
     }
-    .prose h2 a {
-        @apply no-underline transition duration-500 ease-in-out block font-bold border-b border-neutral-600 dark:border-neutral-300;
-        &:hover {
-            @apply text-zinc-600 dark:text-royal-yellow;
-        }
+}
+
+.prose h2 a {
+    @apply no-underline transition duration-500 ease-in-out block font-bold border-b border-neutral-600 dark:border-neutral-300;
+
+    &:hover {
+        @apply text-zinc-600 dark:text-royal-yellow;
     }
-    .prose h3 a {
-        @apply font-semibold no-underline;
-    }
+}
+
+.prose h3 a {
+    @apply font-semibold no-underline;
+}
 </style>
 <script setup lang="ts">
-    useHead({
-        title: "Events",
-        meta: [
-            {
-                name: "description",
-                content: "A list of events in Datronix.",
-            },
-        ],
-    });
-    const route = useRoute();
+useHead({
+    title: "Events",
+    meta: [
+        {
+            name: "description",
+            content: "A list of events in Datronix.",
+        },
+    ],
+});
+const route = useRoute();
 
-    const technical = await queryContent("events/technical-events").find();
-    const nonTechnical = await queryContent(
-        "events/non-technical-events"
-    ).find();
-    const online = await queryContent(
-"events/online-events").find();
+const technical = await queryContent("events/technical-events").find();
+const nonTechnical = await queryContent(
+    "events/non-technical-events"
+).find();
+const onlineEvents = await queryContent(
+    "events/online-events").find();
 </script>
