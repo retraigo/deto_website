b146dac2d4b8056b98e722241ad45e2913bdc29e
NeTT <nett@nett.moe>
2024-02-10 10:17:43 +0530

    reduce font size

--- components/misc/EventBox.vue
+++ components/misc/EventBox.vue
@@ -2,10 +2,10 @@
     <a :key="event._id" :href="event._path" style="background-image:url(/invit_b.webp)"
         class="flex flex-col gap-4 items-center bg-center bg-no-repeat w-full lg:max-w-xl h-60 sm:h-72 bg-contain transition duration-500 ease-in-out transform hover:-translate-y-1"
         data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
-        <div class="uppercase text-black font-semibold text-center font-elementaryGothic text-sm sm:text-lg relative top-16">
+        <div class="uppercase text-black font-semibold text-center font-elementaryGothic text-xs sm:text-sm md:text-lg relative top-16">
             {{ event.title }}
         </div>
-        <div class="text-sm sm:text-lg text-center max-w-60 sm:max-w-xs text-red-900 relative top-[4rem] sm:top-[4.5rem]">
+        <div class="text-xs sm:text-sm md:text-lg text-center max-w-60 sm:max-w-xs text-red-900 relative top-[4rem] sm:top-[4.5rem]">
             {{ event.description }}
         </div>
     </a>
