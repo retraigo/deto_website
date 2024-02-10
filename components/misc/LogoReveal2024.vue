1dad518c7e547e8938de4b330141044a9a096124
NeTT <nett@nett.moe>
2024-02-10 10:37:44 +0530

    update font

--- components/misc/LogoReveal2024.vue
+++ components/misc/LogoReveal2024.vue
@@ -9,34 +9,28 @@
             <div class="main-content bg-fixed">
             </div>
             <div ref="toptext" style=""
-                class="midtext text-sm lg:text-xl transition-opacity duration-500 ease-in-out mt-12 max-w-5xl w-full mx-auto">
+                class="midtext  text-royal-yellow text-sm lg:text-xl transition-opacity duration-500 ease-in-out mt-12 max-w-5xl w-full mx-auto">
                 <div class="absolute top-8 hidden lg:flex flex-row justify-between items-center max-w-5xl w-full">
                     <img src="/EEC_ICON.webp" class="w-28 h-28" />
                     <img src="/icons/2024/ai_logo.webp" class="w-28 h-28" />
                 </div>
-                <span
-                    class="font-loubag uppercase tracking-wider text-xl lg:text-3xl mt-8 bg-gradient-to-b from-yellow-300 to-red-500 text-transparent bg-clip-text">Easwari
+                <span class="font-loubag uppercase tracking-wider text-xl lg:text-3xl mt-8">Easwari
                     Engineering College</span>
-                <br /><span
-                    class="font-loubag uppercase tracking-wider text-xl lg:text-3xl -mt-4 bg-gradient-to-b from-yellow-300 to-red-500 text-transparent bg-clip-text">(Autonomous)</span>
-                <span
-                    class="font-berkshire tracking-wider text-2xl lg:text-3xl mt-4 bg-gradient-to-b from-yellow-300 to-red-500 text-transparent bg-clip-text">Department
+                <br /><span class="font-loubag uppercase tracking-wider text-xl lg:text-3xl -mt-4">(Autonomous)</span>
+                <span class="font-ltfunk tracking-wider text-xl lg:text-3xl mt-4 ">Department
                     of Artificial Intelligence
                     & Data Science</span>
-                    <span
-                    class="font-berkshire tracking-wider text-2xl lg:text-3xl mt-4 bg-gradient-to-b from-yellow-300 to-red-500 text-transparent bg-clip-text">Department
-                    of CSE (AIML)</span>
-                <br /><span
-                    class="font-berkshire tracking-wider text-2xl lg:text-3xl -mt-4 bg-gradient-to-b from-yellow-300 to-red-500 text-transparent bg-clip-text">Presents</span>
+                <br /><span class="font-ltfunk tracking-wider text-xl lg:text-3xl -mt-4">And</span>
+                <span class="font-ltfunk tracking-wider text-xl lg:text-3xl mt-4">Department
+                    of Computer Science & Engineering (AIML)</span>
+                <br /><span class="font-ltfunk tracking-wider text-xl lg:text-3xl -mt-4">Presents</span>
             </div>
-            <div ref="midtext" style=""
-                class="midtext text-5xl md:text-6xl lg:text-8xl lg:mt-7 xl:text-9xl midlogo mt-12">
+            <div ref="midtext" style="" class="midtext text-5xl md:text-6xl lg:text-8xl lg:mt-7 xl:text-9xl midlogo mt-12">
                 <img src="/icons/2024/title.webp" class="w-full text-6xl lg:max-w-2xl font-shadowed" alt="Datronix">
             </div>
             <div ref="midlogo" class="midtext mt-8">
                 <div class="flex flex-col items-center mx-auto relative group">
-                    <img src="/icons/2024/glow_logo_1.webp"
-                        class="w-full lg:w-96 p-5 midlogo relative z-30" />
+                    <img src="/icons/2024/glow_logo_1.webp" class="w-full lg:w-96 p-5 midlogo relative z-30" />
                 </div>
             </div>
         </div>
