8821e3fa64f3601f000295135df1d2c07046506d
NeTT <nett@nett.moe>
2024-03-13 22:17:59 +0530

    update card

--- components/misc/EventBox.vue
+++ components/misc/EventBox.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :key="event._id" :href="event._path" style="background-image:url(/invit_c.webp)"
+    <a :key="event._id" :href="event._path" style="background-image:url(/invit_e.webp)"
         class="flex flex-col gap-4 items-center bg-center bg-no-repeat w-full lg:max-w-xl h-60 sm:h-72 bg-contain transition duration-500 ease-in-out transform hover:-translate-y-1"
         data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="260">
         <div class="uppercase text-black font-semibold text-center font-james text-xs sm:text-sm md:text-lg relative top-16">
